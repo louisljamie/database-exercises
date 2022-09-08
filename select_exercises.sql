e5b51d74bc11295815a2d3dbfa8bbc9fd1e11a4e
louisljamie <louis.l.jamie@gmail.com>
2022-09-08 08:55:23 -0500

    finished

--- select_exercises.sql
+++ select_exercises.sql
@@ -18,17 +18,46 @@
 Write queries to find the following information:
 
 — a. The name of all albums by Pink Floyd
+The Wall
+The Dark Side of the Moon
 
 — b. The year Sgt. Pepper's Lonely Hearts Club Band was released
+1967
 
 — c. The genre for the album Nevermind
+Grunge, Alternative rock
 
 — d. Which albums were released in the 1990s
+The Immaculate Collection
+Metallica
+Dangerous
+Nevermind
+The Bodyguard
+Janes Little Pill
+Falling into You
+Titanic: Music from the Motion Picture
+Let's Talk About Love
+Come On Over
+Supernatural
 
 — e. Which albums had less than 20 million certified sales
+Abbey Road
+Sgt. Pepper's Lonely Hearts Club Band
+Grease: The Original Soundtrack from the Motion Picture
+The Wall
+Born in the U.S.A.
+Brothers in Arms
+Bad
+Dirty Dancing
+The Immaculate Collection
+Nevermind
+Dangerous
+Let's Talk About Love
+Titanic: Music from the Motion Picture
 
 /* f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard 
 rock" or "Progressive rock”? */
+They are considered different by SQL
 
 — Be sure to add, commit, and push your work.
 
