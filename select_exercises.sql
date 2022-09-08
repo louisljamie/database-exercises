e535df2ea0130dbd1f98d150635c76e773f0cd7b
louisljamie <louis.l.jamie@gmail.com>
2022-09-08 08:47:24 -0500

    more answers

---
+++ select_exercises.sql
@@ -0,0 +1,37 @@
+— Use the albums_db database.
+
+— Explore the structure of the albums table.
+
+—a. How many rows are in the albums table?
+31
+
+— b. How many unique artist names are in the albums table?
+23
+
+
+— c. What is the primary key for the albums table?
+
+— d. What is the oldest release date for any album in the albums table? What is the most recent release date?
+1967
+2011
+
+Write queries to find the following information:
+
+— a. The name of all albums by Pink Floyd
+
+— b. The year Sgt. Pepper's Lonely Hearts Club Band was released
+
+— c. The genre for the album Nevermind
+
+— d. Which albums were released in the 1990s
+
+— e. Which albums had less than 20 million certified sales
+
+/* f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard 
+rock" or "Progressive rock”? */
+
+— Be sure to add, commit, and push your work.
+
+
+
+
