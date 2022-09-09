cc73f32e5f78e24ffca121bc33bf38a379635b9c
louisljamie <louis.l.jamie@gmail.com>
2022-09-09 09:32:39 -0500

    finishedv6

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -2,7 +2,7 @@
 'E'. #Use concat() to combine their first and last name
  together as a single column #named full_name. */
 
-
+USE employees;
 SELECT CONCAT(first_name, ' ', last_name) 
 AS full_name
 FROM employees
@@ -12,6 +12,7 @@ LIKE 'e%e';
 
 /* --#Convert the names produced in your last query to all uppercase. */
 
+USE employees;
 SELECT CONCAT(UPPER(first_name), ' ', 
 UPPER(last_name)) 
 AS full_name
@@ -25,7 +26,7 @@ function to #find
 how many days they have been working at the company (Hint: You will also need to 
 #use NOW() or CURDATE()),*/
 
-
+USE employees;
 SELECT first_name, last_name, hire_date, birth_date, 
 DATEDIFF(NOW(), hire_date)
 AS working_dates
@@ -37,9 +38,10 @@ AND birth_date LIKE '%-12-25';
 
 /* --#Find the smallest and largest current salary from the salaries table.*/
 
+USE employees;
 SELECT MIN(salary), MAX(salary)
 FROM salaries
-WHERE to_date = '9999-01-01';
+WHERE to_date > CURDATE();
 
 
 /* #Use your knowledge of built in SQL functions to generate a username for all 
@@ -50,6 +52,7 @@ first 4 characters of the employees last name, an #underscore,
 the month the employee was born, and the last two digits of the year that #they 
 were born. Below is an example of what the first 10 rows will look like */
 
+USE employees;
 SELECT CONCAT(LOWER(left(first_name, 1)), 
 LOWER(left(last_name, 4)), '_', 
 MONTH(birth_date), 
