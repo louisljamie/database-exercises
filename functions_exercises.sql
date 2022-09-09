1be4beac71c99444a73ec9162a01f072e1822f72
louisljamie <louis.l.jamie@gmail.com>
2022-09-09 08:03:07 -0500

    finished

---
+++ functions_exercises.sql
@@ -0,0 +1,51 @@
+/*#Write a query to to find all employees whose last name starts and ends with 
+'E'. #Use concat() to combine their first and last name
+ together as a single column #named full_name.*/
+
+SELECT concat(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%e';
+
+
+
+
+
+--#Convert the names produced in your last query to all uppercase.
+
+SELECT concat(upper(first_name), ' ', upper(last_name)) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%e';
+
+
+/*#Find all employees hired in the 90s and born on Christmas. Use datediff() 
+function to #find 
+how many days they have been working at the company (Hint: You will also need to 
+#use NOW() or CURDATE()),*/
+
+SELECT first_name, last_name, hire_date, birth_date, datediff(now(), hire_date) 
+AS days_working
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE 
+'%-12-25';
+
+
+--#Find the smallest and largest current salary from the salaries table.
+
+SELECT min(salary), max(salary)
+FROM salaries
+WHERE to_date = '9999-01-01';
+
+
+/*#Use your knowledge of built in SQL functions to generate a username for all of 
+the #employees. A username should be all 
+lowercase, and consist of the first character of #the employees first name, the 
+first 4 characters of the employees last name, an #underscore, 
+the month the employee was born, and the last two digits of the year that #they 
+were born. Below is an example of what the first 10 rows will look like*/
+
+SELECT concat(lower(left(first_name, 1)), lower(left(last_name, 4)), '_', 
+month(birth_date), right(year(birth_date), 2)) AS username
+FROM employees
+LIMIT 10;
+
+
