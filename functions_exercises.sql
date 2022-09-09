48395c9342860121aa6e4b7cd988e17385ddbda5
louisljamie <louis.l.jamie@gmail.com>
2022-09-09 09:27:42 -0500

    finishedv5

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -30,7 +30,8 @@ SELECT first_name, last_name, hire_date, birth_date,
 DATEDIFF(NOW(), hire_date)
 AS working_dates
 FROM employees
-WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
+WHERE hire_date LIKE '199%' 
+AND birth_date LIKE '%-12-25';
 
 
 
