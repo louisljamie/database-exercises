5927099c37df57a0bed753526f36b2a9a9c81b3a
louisljamie <louis.l.jamie@gmail.com>
2022-09-10 14:32:02 -0500

    had to copy over files, they were in the wrong directory

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,33 +1,80 @@
-— Use the albums_db database.
+—- 2. Use the albums_db database.
 
-— Explore the structure of the albums table.
+USE albums_db;
 
-—a. How many rows are in the albums table?
-31
+—- 3. Explore the structure of the albums table.
 
-— b. How many unique artist names are in the albums table?
-23
+SHOW tables;
+DESCRIBE albums;
+
+—- a. How many rows are in the albums table?
+
+SELECT COUNT(*) 
+FROM albums;
+
+--31
+
+—- b. How many unique artist names are in the albums table?
+
+SELECT COUNT(DISTINCT artist)
+FROM albums;
+
+--23
 
 
-— c. What is the primary key for the albums table?
+—- c. What is the primary key for the albums table?
 
-— d. What is the oldest release date for any album in the albums table? What is the most recent release date?
-1967
-2011
+SELECT id  
+FROM albums;
 
-Write queries to find the following information:
 
-— a. The name of all albums by Pink Floyd
+-— d. What is the oldest release date for any album in the albums table? What is the most recent release date?
+
+SELECT MIN(release_date), MAX(release_date)
+FROM albums;
+
+-- 1967
+-- 2011
+
+/* 4. Write queries to find the following information: */
+/* -— a. The name of all albums by Pink Floyd */
+
+SELECT name
+FROM albums
+WHERE artist = 'Pink Floyd';
+
+/*
 The Wall
 The Dark Side of the Moon
+*/
 
-— b. The year Sgt. Pepper's Lonely Hearts Club Band was released
+/* —- b. The year Sgt. Pepper's Lonely Hearts Club Band was released */
+
+SELECT release_date
+FROM albums
+WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+/* 
 1967
+*/
 
-— c. The genre for the album Nevermind
+/*—- c. The genre for the album Nevermind */
+
+SELECT genre
+FROM albums
+WHERE name = 'Nevermind';
+
+*/
 Grunge, Alternative rock
+*/
 
-— d. Which albums were released in the 1990s
+/*—- d. Which albums were released in the 1990s */
+
+SELECT name, release_date
+FROM albums
+WHERE release_date BETWEEN 1990 AND 1999;
+
+/* 
 The Immaculate Collection
 Metallica
 Dangerous
@@ -38,9 +85,16 @@ Falling into You
 Titanic: Music from the Motion Picture
 Let's Talk About Love
 Come On Over
-Supernatural
+Supernatural 
+*/
 
-— e. Which albums had less than 20 million certified sales
+—- e. Which albums had less than 20 million certified sales
+
+SELECT name, certified_sales
+FROM albums
+WHERE certified_sales < 20000000;
+
+/* 
 Abbey Road
 Sgt. Pepper's Lonely Hearts Club Band
 Grease: The Original Soundtrack from the Motion Picture
@@ -53,13 +107,37 @@ The Immaculate Collection
 Nevermind
 Dangerous
 Let's Talk About Love
-Titanic: Music from the Motion Picture
+Titanic: Music from the Motion Picture 
+*/
 
 /* f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard 
 rock" or "Progressive rock”? */
-They are considered different by SQL
 
-— Be sure to add, commit, and push your work.
+-- They are considered different by SQL
+
+SELECT name
+FROM albums
+WHERE genre = 'Rock';
+
+SELECT name
+FROM albums
+WHERE genre = '%rock%';
+
+/*
+
+The Wall
+Abbey Road
+Sgt. Pepper's Lonely Hearts Club Band
+The Dark Side of the Moon
+The Bodyguard
+Let's Talk About Love
+Titanic: Music from the Motion Picture
+Come On Over
+Supernatural
+
+*/
+
+
 
 
 
--- where_exercises.sql
+++ where_exercises.sql
@@ -1,23 +1,140 @@
+-- 1. Create a file named where_exercises.sql. Make sure to use the employees database.
 
-—2
-709
-—3
-YES
-—4
-180421
-—5
-7330
-—6
-24292
-—7
-899
-—8
+USE employees;
 
-—9
-135214
-—10
-789
-—11
-346
-—12
-1873
+-- 2. Find all employees with first names 'Irena', 'Vidya', or 'Maya' using IN. Enter a comment with the number of records returned.
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+--709
+
+/* 3. Find all employees with first names 'Irena', 'Vidya', or 'Maya', as in Q2, but use OR instead of IN. 
+Enter a comment with the number of records returned. Does it match number of rows from Q2? (709 observations; it matches.) */
+
+SELECT *
+FROM employees
+WHERE first_name = 'Irena' 
+    OR first_name = 'Vidya' 
+    OR first_name =  'Maya';
+
+-- YES
+
+-- 4. Find all employees with first names 'Irena', 'Vidya', or 'Maya', using OR, AND who is male. Enter a comment with the number of records returned.
+
+FROM employees
+WHERE gender = 'M'
+AND 
+    (first_name = 'Irena' 
+     OR first_name = 'Vidya' 
+     OR first_name = 'Maya');
+
+--441
+-- or 
+
+SELECT *
+FROM employees
+WHERE 
+    (first_name = 'Irena' 
+     OR first_name = 'Vidya' 
+     OR first_name = 'Maya')
+AND gender = 'M';
+
+-- 5. Find all employees whose last name starts with 'E'. Enter a comment with the number of employees whose last name starts with E. 
+
+SELECT *
+FROM employees 
+WHERE last_name LIKE 'E%';
+
+--7330 
+
+-- 6. Find all employees whose last name starts OR ends with 'E'. Enter a comment with the number of employees whose last name starts or ends with E.
+
+SELECT *
+FROM employees 
+WHERE last_name LIKE 'E%' 
+OR last_name LIKE '%E';
+
+--30723
+
+-- How many employees have a last name that ends with E, but does not start with E? ( observations)
+
+SELECT *
+FROM employees 
+WHERE last_name LIKE '%E' 
+AND NOT last_name LIKE 'E%';
+
+--23_393
+
+-- 7. Find all employees employees whose last name starts AND ends with 'E'. Enter a comment with the number of employees whose last name starts and ends with E. 
+
+SELECT *
+FROM employees 
+WHERE last_name LIKE 'E%' 
+AND last_name LIKE '%E';
+
+--899
+
+-- We can also write our query like this... 
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%E';
+
+--899
+
+-- How many employees' last names end with E, regardless of whether they start with E?
+
+SELECT *
+FROM employees 
+WHERE last_name LIKE '%E';
+
+--24292
+
+-- 8. Find all employees hired in the 90s. Enter a comment with the number of employees returned.
+
+SELECT * 
+FROM employees
+WHERE hire_date LIKE '199%';
+
+--135214 
+
+-- or
+
+SELECT * 
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+-- 9. Find all employees born on Christmas. Enter a comment with the number of employees returned. 
+SELECT *
+FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+--842 
+
+-- 10. Find all employees hired in the 90s AND born on Christmas. Enter a comment with the number of employees returned.
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25';
+
+--362
+
+-- 11. Find all employees with a 'q' in their last name. Enter a comment with the number of records returned.
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%';
+
+-- 1873
+
+-- 12. Find all employees with a 'q' in their last name but NOT 'qu'. How many employees are found? ( observations)
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%' 
+AND NOT last_name LIKE '%qu%';
+
+-- 547
+
