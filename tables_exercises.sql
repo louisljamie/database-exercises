c3107cf88c8f5df662b0cc6158776fe9bddceff7
louisljamie <louis.l.jamie@gmail.com>
2022-09-10 13:26:48 -0500

    cleaned up code for reference later

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -1,12 +1,13 @@
--- 1 Open MySQL Workbench and login to the database server 
--- 2 Save your work in a file named tables_exercises.sql 
--- 3 Use the employees database. Write the SQL code necessary to do this. 
+-- 2 Use the employees database. Write the SQL code necessary to do this. 
+
 USE employees;
 
--- 4 List all the tables in the database. Write the SQL code necessary to accomplish this.
-SHOW TABLES;
+-- 3 List all the tables in the database. Write the SQL code necessary to accomplish this.
 
-SHOW TABLES FROM employees;
+
+
+SHOW tables
+FROM employees;
 
 -- 5 Explore the employees table. What different data types are present on this table?
 
@@ -22,6 +23,7 @@ varchar(16)
 
 -- 6 Which table(s) do you think contain a numeric type column? (Write this question and your answer in a comment)
 -- saleries
+
 /*
 dept_emp
 dept_manager
@@ -29,7 +31,9 @@ employees
 salaries
 titles
 */
+
 -- 7 Which table(s) do you think contain a string type column? (Write this question and your answer in a comment)
+
 /* 
 departments
 dept_emp
@@ -39,6 +43,7 @@ titles
  */
 
 -- 8 Which table(s) do you think contain a date type column? (Write this question and your answer in a comment)
+
 /*
 dept_emp
 dept_manager
@@ -49,13 +54,14 @@ titles
 */
 
 -- 9 What is the relationship between the employees and the departments tables? (Write this question and your answer in a comment)
+
 -- There is no relation between the two. The information which links them is in the dept_emp
 
 -- 10 Show the SQL that created the dept_manager table. Write the SQL it takes to show this as your exercise solution.
 
 SHOW CREATE TABLE dept_manager;
 
-CREATE TABLE `dept_manager` (
+/* CREATE TABLE `dept_manager` (
   `emp_no` int NOT NULL,
   `dept_no` char(4) NOT NULL,
   `from_date` date NOT NULL,
@@ -64,4 +70,4 @@ CREATE TABLE `dept_manager` (
   KEY `dept_no` (`dept_no`),
   CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE ON UPDATE RESTRICT,
   CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE ON UPDATE RESTRICT
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 */
